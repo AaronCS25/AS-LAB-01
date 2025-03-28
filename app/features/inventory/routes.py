a2e51df5fb17b0919f4b95111b80ecea491aa69a
Mauricio Alvarez <mauricio.alvarez@utec.edu.pe>
2025-03-27 21:28:15 -0500

    start routes Mauricio

--- app/features/inventory/routes.py
+++ app/features/inventory/routes.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/inventory", tags=["inventory"])
 
-@router.get("/")
+@router.get("/add/{id}")
 def root():
+
+    return {"message": "Inventory Module"}
+
+@router.get("/update/{id}")
+def root():
+    
     return {"message": "Inventory Module"}
